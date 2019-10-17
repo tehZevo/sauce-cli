ce05ae9f98700ccc5bb568109f49450086925724
tehZevo <tehzevo@users.noreply.github.com>
2019-10-16 23:41:55 -0400

    Handle redirects

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-var request = require("request");
+var rp = require("request-promise");
 var minimist = require("minimist");
+var cheerio = require("cheerio");
+var UserAgent = require("user-agents");
 
 function iflUrl(query)
 {
   return encodeURI(`https://www.google.com/search?q=${query}&btnI`);
 }
 
-module.exports = () =>
+async function doRequest(url, options)
 {
-  const args = minimist(process.argv.slice(2))
+  options.uri = url;
+  var html = await rp(options);
+  var $ = cheerio.load(html);
+
+  var title = $("title").text();
+  //if redirect page
+  if(title == "Redirect Notice")
+  {
+    //grab first link
+    var redirectUrl = $("a").attr("href");
+    //surf link
+    return await doRequest(redirectUrl, options);
+  }
+
+  //otherwise return html
+  return html;
+}
+
+module.exports = async () =>
+{
+  var args = minimist(process.argv.slice(2));
+  var agent = new UserAgent().toString();
+  var options = {
+    headers: {'user-agent': agent}
+  }
 
   if(args._.length < 1)
   {
@@ -20,15 +46,8 @@ module.exports = () =>
 
   if (args.output || args.o)
   {
-    request(url, function (err, res, body)
-    {
-      if(err)
-      {
-        throw err;
-      }
-
-      console.log(body);
-    });
+    var html = await doRequest(url, options);
+    console.log(html);
   }
   else
   {
